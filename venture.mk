56003e8340e3a086008726bf96b933683c1b34ff
Josh Chasky <jchasky@gmail.com>
2014-12-07 10:00:05 -0500

    Flo: Add secure erase, update build prop override, and QCOM_BIONIC flags
    Change-Id: I54bb7819edc923c4c5e17368f12524f78258073e
    
    Conflicts:
    
    	venture.mk

--- venture.mk
+++ venture.mk
@@ -14,10 +14,10 @@ $(call inherit-product, device/asus/flo/full_flo.mk)
 PRODUCT_DEVICE := flo
 PRODUCT_NAME := venture_flo
 PRODUCT_BRAND := Google
-PRODUCT_MODEL := Nexus 7
-PRODUCT_MANUFACTURER := Asus
+PRODUCT_MODEL := Nexus 7 2013
+PRODUCT_MANUFACTURER := ASUS
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/flo:4.4.4/KTU84P/1227136:user/release-keys PRIVATE_BUILD_DESC="razor-user 4.4.4 KTU84P 1227136 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/flo:5.0.1/LRX22C/1227136:user/release-keys PRIVATE_BUILD_DESC="razor-user 5.0.1 LRX22C 1227136 release-keys"
